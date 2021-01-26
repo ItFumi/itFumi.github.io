5d299b92e674e57374428b20dd9a926e49f7c2c5
Ito Fumitaka <ffffffiffffffffffff@yahoo.co.jp>
2021-01-26 18:14:15 +0900

    change introduction and add analysis_report

--- js/main.js
+++ js/main.js
@@ -2,53 +2,45 @@
 	// header nav scroll
 	$("nav ul li a").click(function () {
 		var menu_num = $("nav ul li a").index(this),
-        	menu_position = $(".toScroll").eq(menu_num).offset().top;
-        if(menu_num == 0) {
-        	$("html,body").animate({ scrollTop: 0 },800);
-        } else {
-        	$("html,body").animate({ scrollTop: menu_position },800);
+    menu_position = $(".toScroll").eq(menu_num).offset().top;
+    if(menu_num == 0) {
+    	$("html,body").animate({ scrollTop: 0 },800);
+    } else {
+    	$("html,body").animate({ scrollTop: menu_position },800);
 		}
-    });
+  });
 	// top scroll
 	$(".toTop a").click(function () {
-        $("html,body").animate({ scrollTop: 0 },800);
-    });
-    // toScroll position
-    var window_height = $(window).height(),
-    	toScroll_position = [];
-    $(".toScroll").each(function () {
-    	var target_position = $(this).offset().top;
-    	toScroll_position.push(target_position-window_height);
-    });
+      $("html,body").animate({ scrollTop: 0 },800);
+  });
+  // skill bar move position
+  var window_height = $(window).height(),
+  skill_position = $("#skill_list").offset().top,
+  trigger_position = skill_position-window_height;
 	// scroll event
 	$(window).scroll(function () {
 		// live scroll
 		var scrollTop = $(window).scrollTop();
 		// skill
-		if(scrollTop > toScroll_position[1]) {
+		if(scrollTop > trigger_position) {
 			skill_bar();
-		}
-		// works
-		if(scrollTop > toScroll_position[3]) {
-			
 		}
 	});
 	// skill bar
 	var toExecutableOnce = function(f){
-    	var called = false, result = undefined;
-    	return function(){
-        	if(!called){
-            	result = f.apply(this, arguments);
-            	called = true;
-        	}   
-        	return result;
-    	};  
+    var called = false;
+    return function(){
+      if(!called){
+        f.apply();
+				called = true;
+    	}
+    };
 	};
 	var skill_bar = toExecutableOnce(function () {
-    	$(".skill_bar_w").each(function () {
-    		var self = this,
-    			percentage = $(this).data("percentage"),
-    			parent = $(this).parents(".skill_box");
+    $(".skill_bar_w").each(function () {
+    	var self = this,
+    	percentage = $(this).data("percentage"),
+    	parent = $(this).parents(".skill_box");
 			$({ Counter: 0 }).animate({ Counter: percentage }, {
 				duration: 5000,
 				easing: "swing",
